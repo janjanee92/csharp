441df6d01ee1fef236563026c68a734f4fdfa0f6
rowanlee92 <watrv41@gmail.com>
2021-04-05 13:30:03 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Program 
+    { 
+        static int Add(int a, int b, int c = 0, float f = 1.0f, double d = 3.0)
+        {
+            return a + b + c;
+        }
+
+        static void Main(string[] args)
+        {
+            Add(1, 2, d: 2.0);
+        }
+    }
+}
