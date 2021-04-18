4a261ec01f8bad6626066a3fd5edfdc81c3df917
rowanlee92 <watrv41@gmail.com>
2021-04-19 00:18:46 +0900

    Update: OOP

--- Program.cs
+++ Program.cs
@@ -3,70 +3,23 @@
 namespace CSharp
 {
 
-    class Player
-    {
-        static public int counter = 1;
-
-        public int id;
-        public int hp;
-        public int attack;
-
-        public Player()
-        {
-            Console.WriteLine("플레이어 생성자 호출!");
-        }
-
-        public Player(int hp)
-        {
-            this.hp = hp;
-            Console.WriteLine("플레이어 hp 생성자 호출");
-        }
-        public void Move()
-        {
-            Console.WriteLine("Player Move");
-        }
-
-        public void Attack()
-        {
-            Console.WriteLine("Player Attack");
-        }
-
-    }
-    class Mage : Player
-    {
-
-    }
-
-    class Archer : Player
-    {
-        
-    }
-
-    // Ref
-    class Knight : Player
-    {
-
-        public Knight() : base(100)
-        {
-            base.attack = 20;
-            Console.WriteLine("Knight 생성자 호출");
-        }
-
-        static public Knight CreateKnight()
-        {
-            Knight knight = new Knight();
-            knight.hp = 100;
-            return knight;
-        }
-
-    }
-
     class Program 
     {
+
         static void Main(string[] args)
         {
-            Knight k = Knight.CreateKnight();
-            k.Move();
+            string name = "Harry Potter";
+
+            bool found = name.Contains("Harry");
+            int index = name.IndexOf('P');
+
+            string lowerCaseName = name.ToLower();
+            Console.WriteLine(lowerCaseName);
+
+            name = name.Replace('a', 'z');
+
+            string[] names = name.Split(new char[] { ' ' });
+            string sub = name.Substring(5);
         }
     }
 }
