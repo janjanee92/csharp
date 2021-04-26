76ea0ac49ea91f91de94f712416ce3d65b867157
rowanlee92 <watrv41@gmail.com>
2021-04-26 22:14:24 +0900

    Add: Text RPG 2

---
+++ Creature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp
+{
+    public enum CreatureType
+    {
+        None,
+        Player = 1,
+        Monster = 2
+    }
+    class Creature
+    {
+        protected CreatureType ctype = CreatureType.None;
+
+        protected int hp = 0;
+        protected int attack = 0;
+
+        protected Creature(CreatureType type)
+        {
+            ctype = type;
+        }
+
+        public void SetInfo(int hp, int attack)
+        {
+
+            this.hp = hp;
+            this.attack = attack;
+        }
+        public int GetHp() { return hp; }
+        public int GetAttack() { return attack; }
+        public bool IsDead() { return hp <= 0; }
+        public void OnDamaged(int damage)
+        {
+            hp -= damage;
+            if (hp < 0)
+                hp = 0;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,18 +8,14 @@ namespace CSharp
 
         static void Main(string[] args)
         {
-            string name = "Harry Potter";
+            Game game = new Game();
 
-            bool found = name.Contains("Harry");
-            int index = name.IndexOf('P');
-
-            string lowerCaseName = name.ToLower();
-            Console.WriteLine(lowerCaseName);
-
-            name = name.Replace('a', 'z');
-
-            string[] names = name.Split(new char[] { ' ' });
-            string sub = name.Substring(5);
+            while (true)
+            {
+                game.Process();
+            }
         }
     }
+
 }
+ 
